c3985a55525a2efe62b679e5edb48b7b129639c5
olexiisoroka <oleksii.soroka@itomy.ch>
2022-03-15 19:03:36 +0200

    Fifth commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,15 +2,12 @@ package com.example.myapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.math.Ma
 import com.example.math.TestClass
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Ma().sumOfInt(7, 77)
-        TestClass().doingTest()
+        TestClass().doingTest(this)
     }
 }
--- math/src/main/java/com/example/math/TestClass.kt
+++ math/src/main/java/com/example/math/TestClass.kt
@@ -1,8 +1,11 @@
 package com.example.math
 
+import android.content.Context
+import android.widget.Toast
+
 class TestClass {
 
-    fun doingTest() {
-        print("The test success")
+    fun doingTest(context: Context) {
+        Toast.makeText(context, "The test pass successful", Toast.LENGTH_SHORT).show()
     }
 }
