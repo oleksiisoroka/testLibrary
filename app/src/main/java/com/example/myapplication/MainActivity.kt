9e176b3cde462cfc8bc022b88f93db5146cb2871
olexiisoroka <oleksii.soroka@itomy.ch>
2022-03-15 17:51:37 +0200

    Third commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.myapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.math.sumOfInt
+import com.example.math.Ma
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sumOfInt(7, 77)
+        Ma().sumOfInt(7, 77)
     }
 }
--- math/src/main/java/com/example/math/Ma.kt
+++ math/src/main/java/com/example/math/Ma.kt
@@ -1,7 +1,11 @@
 package com.example.math
 
+class Ma {
 
-fun sumOfInt(i1: Int, i2: Int): Int {
-    println("The sum of two Integers is ${i1 + i2}")
-    return i1 + i2
+    open fun sumOfInt(i1: Int, i2: Int): Int {
+        println("The sum of two Integers is ${i1 + i2}")
+        return i1 + i2
+    }
+
 }
+
